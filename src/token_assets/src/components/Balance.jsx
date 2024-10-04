b133d239c7a65f286d7ef48023f53616fa77eb77
a-levitt <artemis.levitt@gmail.com>
2024-10-04 14:54:06 +0200

    Check balance on frontend

--- src/token_assets/src/components/Balance.jsx
+++ src/token_assets/src/components/Balance.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { Principal } from "@dfinity/principal";
+import { token } from "../../../declarations/token";
 
 function Balance() {
+
+  const [inputValue, setInput] = useState("");
+  const [balanceResult, setBalance] = useState("");
   
   async function handleClick() {
-    console.log("Balance Button Clicked");
-  }
+    // console.log(inputValue);
+    const principal = Principal.fromText(inputValue);
+    const balance = await token.balanceOf(principal);
+    setBalance(balance.toLocaleString());
+  };
 
 
   return (
@@ -15,6 +23,8 @@ function Balance() {
           id="balance-principal-id"
           type="text"
           placeholder="Enter a Principal ID"
+          value={inputValue}
+          onChange={(e) => setInput(e.target.value)}
         />
       </p>
       <p className="trade-buttons">
@@ -25,7 +35,7 @@ function Balance() {
           Check Balance
         </button>
       </p>
-      <p>This account has a balance of ALVTT.</p>
+      <p>This account has a balance of {balanceResult}.</p>
     </div>
   );
 }
