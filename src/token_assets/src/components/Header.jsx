f8c6eb6e9566ab205984593ebdad613ebf194f11
a-levitt <artemis.levitt@gmail.com>
2024-10-04 10:54:23 +0200

    Create coin,amount; changes in frontend data

--- src/token_assets/src/components/Header.jsx
+++ src/token_assets/src/components/Header.jsx
@@ -6,9 +6,9 @@ function Header() {
       <div className="blue window" id="logo">
         <h1>
           <span role="img" aria-label="tap emoji">
-            💎
+            🪙
           </span>
-          DSurv
+          DLvtt
         </h1>
       </div>
     </header>
